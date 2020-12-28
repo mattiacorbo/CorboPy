614e2b22dbd27ca845479433cfb3edcb92f79627
mattiacorbo <74246847+mattiacorbo@users.noreply.github.com>
2020-12-28 20:09:22 +0100

    Delete Esercizio python n. 300000.py

--- Esercizio python n. 300000.py
+++
@@ -1,6 +0,0 @@
-#Esercizio - stringhe
-stringa=input("Digita quello che vuoi ")
-Lettere = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
-for lettera in Lettere:
-    if stringa.count(lettera) > 0:
-        print(lettera, " = ", stringa.count(lettera))
